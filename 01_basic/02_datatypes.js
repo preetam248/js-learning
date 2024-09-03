2ab997069ffacf2f9c092f6927788cb687b13a23
preetam248 <154631484+preetam248@users.noreply.github.com>
2024-09-03 06:46:07 +0000

    learn about datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //not run coz we are using nodejs, not browser
+
+let name = "Preetam";
+let age = 25
+let isLoggedIn = true
+
+let address = null
+
+// console.table([name, age, isLoggedIn, address])
+
+console.log(typeof "bittu");
+
+/*number=> 2 to power 53
+big int
+string=>""
+boolean=> true/ false
+null standalone value , is a object
+undefined is a type
+symbol=> unique
+
+object*/
+
