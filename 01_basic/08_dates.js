08b11d72c63034fe248f738f83ecb963caf89e67
preetam248 <154631484+preetam248@users.noreply.github.com>
2024-09-03 19:03:41 +0000

    learn date class function

---
+++ 01_basic/08_dates.js
@@ -0,0 +1,42 @@
+let myDate = new Date()
+
+// console.log(typeof myDate); //object
+
+
+// console.log(myDate)
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toTimeString())
+// console.log(myDate.toISOString())
+// console.log(myDate.toJSON())
+// console.log(myDate.toLocaleString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleTimeString())
+
+// let myCreatedDate = new Date(2024,8,3) //montrh index from 0
+// console.log(myCreatedDate.toDateString());
+
+// let myCreatedDate = new Date(2024,8,3,23,54) //montrh index from 0
+// console.log(myCreatedDate.toLocaleString());
+
+// let myCreatedDate = new Date("03/08/2024 23:55") //montrh index from 1
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp); //milisec
+// // console.log(myCreatedDate.getTime());
+// console.log(Math.floor(myTimeStamp/1000)); //sec
+
+let newDate = new Date();
+// console.log(newDate.getDate());
+// console.log(newDate.getDay());//day of week, mon-1
+// console.log(newDate.getTime());
+// console.log(newDate.getSeconds());
+console.log(newDate.getDate());
+
+newDate.toLocaleString("default",{
+    dateStyle : "long"
+})
+// console.log(newDate.toLocaleString());
+
+
