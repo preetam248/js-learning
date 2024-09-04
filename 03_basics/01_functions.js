ee1ebe725111ee1818302e71c613d988ea2be9ea
preetam248 <154631484+preetam248@users.noreply.github.com>
2024-09-04 12:01:29 +0000

    learn functions

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,38 @@
+// function sayMyName (myName) {
+//     console.log(myName)
+// }
+
+// sayMyName("Rajoshree")
+
+// function addTwoNumbers(num1, num2) {
+//     console.log(num1 + num2)
+// }
+
+// // addTwoNumbers(3,7)
+
+// function multiply(num1, num2) {
+//     return num1 * num2
+// }
+
+// let result = multiply(5,7)
+// // console.log(result)
+
+// function loginUserMsg(userName) {
+//     if(userName === undefined){
+//         console.log("enter user name")
+//         return
+//     }
+//     return `${userName} just login`
+// }
+
+// console.log(loginUserMsg("hitesh"))
+
+// function loginUserMsg(userName = "Sam") { //atleast = Sam
+//     if(userName === undefined){
+//         console.log("enter user name")
+//         return
+//     }
+//     return `${userName} just login`
+// }
+
+// console.log(loginUserMsg())
