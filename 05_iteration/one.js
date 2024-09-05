d8f2c39169fa6452985105a741e851651ee98949
preetam248 <154631484+preetam248@users.noreply.github.com>
2024-09-05 10:43:55 +0000

    loops

---
+++ 05_iteration/one.js
@@ -0,0 +1,22 @@
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     console.log(element);
+// }
+let arr = [12,52,32,48]
+arr.forEach(element => {
+    console.log(element)
+})
+
+let i = 0
+while(i<10){
+    console.log(i)
+    i++
+}
+
+let j = 10
+do{
+    console.log(j)
+    j--
+}while(j>0)
+
+
