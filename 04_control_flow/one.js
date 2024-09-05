23c7148cd15b0a30d26dcbf27b4bcba7fd43b878
preetam248 <154631484+preetam248@users.noreply.github.com>
2024-09-05 07:03:01 +0000

    learncontrol flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,43 @@
+let age = 25;
+
+// if(age <= 50){
+//     console.log(`age less than 50`)
+// }else{
+//     console.log(`age greater than 50`)
+
+// }
+
+// if (age <= 50) {
+//     console.log(`age less than 50`)
+// }else if (age<=100){
+//     console.log(`age greater than 50 and less than 100`)
+
+// }else{
+//     console.log(`age greater than 100`)
+// }
+
+// if (age <=50 && age>=10){
+//     console.log(`age between 10-50`)
+// }
+
+// let month = 2;
+
+// switch (month) {
+//   case 1:
+//     console.log(`Jan`)
+//     console.log(`january`)
+//     break;
+//   case 2:
+//     console.log(`Feb`)
+//     console.log(`February`)
+//     break;
+//   case 3:
+//     console.log(`Mar`);
+//     console.log(`March`);
+//     break;                 //if not has break statment, execute all statement after match except default statement
+//   case 4:
+//     console.log(`Apr`)
+//     console.log(`April`)
+//     break;
+//     default : console.log("Not a valid month");
+// }
